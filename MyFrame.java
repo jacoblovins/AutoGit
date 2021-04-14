3c112905a298c327d4f7967242d192fd0158718d
jacoblovins <lovins.jacob@yahoo.com>
2021-04-14 15:14:52 -0400

    when submit is pressed, shell command is run

--- MyFrame.java
+++ MyFrame.java
@@ -6,12 +6,16 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MyFrame extends JFrame implements ActionListener {
 
     JButton button;
     JTextField textField;
-    MyFrame(){
+
+    MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
@@ -19,7 +23,7 @@ public class MyFrame extends JFrame implements ActionListener {
         button.addActionListener(this);
 
         textField = new JTextField();
-        textField.setPreferredSize(new Dimension(250,40));
+        textField.setPreferredSize(new Dimension(250, 40));
 
         this.add(button);
         this.add(textField);
@@ -29,10 +33,37 @@ public class MyFrame extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==button){
-            System.out.println("Welcome " + textField.getText());
+        if (e.getSource() == button) {
+            // System.out.println("Welcome " + textField.getText());
+            try {
+                Process process = Runtime.getRuntime().exec("ls -l /");
+
+                StringBuilder output = new StringBuilder();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    output.append(line + "\n");
+                }
+
+                int exitVal = process.waitFor();
+                if (exitVal == 0) {
+                    System.out.println("Success");
+                    System.out.println(output);
+                    System.exit(0);
+                } else {
+                    System.out.println("something went wrong!");
+                }
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (InterruptedException e2) {
+                e2.printStackTrace();
+            }
         }
-        
+
     }
-    
+
 }
