95b06d4d539db2357c563f3873821dc6c58d9f6f
jacoblovins <lovins.jacob@yahoo.com>
2021-04-14 22:40:26 -0400

    added commit button- not working

--- MyFrame.java
+++ MyFrame.java
@@ -11,7 +11,8 @@ import java.io.IOException;
 
 public class MyFrame extends JFrame implements ActionListener {
 
-    JButton button;
+    JButton stageButton;
+    JButton commitButton;
     JTextField textField1;
     JTextField textField2;
 
@@ -19,33 +20,39 @@ public class MyFrame extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
-        button = new JButton("Submit");
-        button.addActionListener(this);
+        stageButton = new JButton("Stage");
+        stageButton.addActionListener(this);
 
-        textField1 = new JTextField();
+        commitButton = new JButton("Commit");
+        commitButton.addActionListener(this);
+
+        textField1 = new JTextField("repositories/day_planner");
         textField1.setPreferredSize(new Dimension(250, 40));
-        textField2 = new JTextField();
+        textField2 = new JTextField("added something");
         textField2.setPreferredSize(new Dimension(250, 40));
 
-        this.add(button);
+        this.add(stageButton);
         this.add(textField1);
+        this.add(commitButton);
         this.add(textField2);
         this.pack();
         this.setVisible(true);
     }
 
+    
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == button) {
-            // String command = "cd /Users/jacoblovins/Desktop/" + textField1.getText() + " && git add -A";
-            File dir = new File("/Users/jacoblovins/Desktop/" + textField1.getText());
+        File dir = new File("/Users/jacoblovins/Desktop/" + textField1.getText());
+        if (e.getSource() == stageButton) {
+            // String command = "cd /Users/jacoblovins/Desktop/" + textField1.getText() + "
+            // && git add -A";
             System.out.println(dir);
-            
+
             try {
                 Process stage = Runtime.getRuntime().exec("git add -A", null, dir);
 
-                int exitVal = stage.waitFor();
-                if (exitVal == 0) {
+                int exitVal1 = stage.waitFor();
+                if (exitVal1 == 0) {
                     System.out.println("Successfully Staged");
                     // System.exit(0);
                 } else {
@@ -57,25 +64,24 @@ public class MyFrame extends JFrame implements ActionListener {
             } catch (InterruptedException e2) {
                 e2.printStackTrace();
             }
+        } else if (e.getSource() == commitButton) {
+            try {
+                Process commit = Runtime.getRuntime().exec("git commit -m \"added something\"", null, dir);
 
-            // try {
-            //     Process commit = Runtime.getRuntime().exec("git commit -m '" + textField2.getText() + "'", null, dir);
+                int exitVal = commit.waitFor();
+                if (exitVal == 0) {
+                    System.out.println("Successfully committed");
+                    // System.exit(0);
+                } else {
+                    System.out.println(exitVal);
+                    System.out.println("something went wrong in committing!");
+                }
 
-            //     int exitVal = commit.waitFor();
-            //     if (exitVal == 0) {
-            //         System.out.println("Successfully committed");
-            //         // System.exit(0);
-            //     } else {
-            //         System.out.println("something went wrong in committing!");
-            //     }
-
-            // } catch (IOException e1) {
-            //     e1.printStackTrace();
-            // } catch (InterruptedException e2) {
-            //     e2.printStackTrace();
-            // }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (InterruptedException e2) {
+                e2.printStackTrace();
+            }
         }
-
     }
-
 }
